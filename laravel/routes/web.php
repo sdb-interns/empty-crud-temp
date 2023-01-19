50ff24f8ac4d7e0412e15d5e3cfae74862d073a3
SATOTOMI <tomita@i.social-db.co.jp>
2023-01-19 14:53:35 +0900

    userに関するcrudを作成した

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +12,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+use App\Http\Controllers\UserController;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('users', [UserController::class,'index'])->name('user.index');
+Route::get('users/create', [UserController::class,'create'])->name('user.create');
+Route::post('users/', [UserController::class,'store'])->name('user.store');
+Route::get('users/{id}', [UserController::class,'edit'])->name('user.edit');
+Route::post('users/{id}', [UserController::class,'update'])->name('user.update');
+Route::delete('users/{id}', [UserController::class,'delete'])->name('user.delete');
